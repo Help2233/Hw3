8f12a2d5112bc6171a2ecaf6f83d05285b1c08f2
help1 <help13341@gmail.com>
2025-02-02 16:02:02 +0400

    ООП

---
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -0,0 +1,56 @@
+package org.skypro.skyshop.basket;
+
+import org.skypro.skyshop.product.Product;
+
+public class ProductBasket {
+    Product[] products = new Product[5];
+
+    public void addProduct(Product product) {
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] == null) {
+                products[i] = product;
+                return;
+            }
+        }
+        System.out.println("Места нет");
+    }
+
+    public int getAllPrice() {
+        int price = 0;
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] != null) {
+                price = price + products[i].getCost();
+            }
+        }
+        return price;
+    }
+
+    public boolean isProductInCart(String productName) {
+        for (Product product : products) {
+            if (product.getProduct().equalsIgnoreCase(productName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void clearCart() {
+        for (int i = 0; i < products.length; i++) {
+            products[i] = null;
+        }
+        products.clone(); // Чистим список
+    }
+
+    public void printCart() {
+    for (Product product : products ){
+        if (product != null) {
+            System.out.println(product.getProduct());
+        } else {
+            System.out.println("null");
+        }
+    }
+}
+}
+
+
+
---
+++ src/org/skypro/skyshop/product/Product.java
@@ -0,0 +1,31 @@
+package org.skypro.skyshop.product;
+
+public class Product {
+    private String product;
+    private int cost;
+
+
+    public int getCost() {
+        return cost;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public Product() {
+    }
+
+    public Product(String product, int cost) {
+        this.product = product;
+        this.cost = cost;
+    }
+}
