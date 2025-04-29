ffcabe4b6b299ebb013ed20dc15aae3af5533cfb
help1 <help13341@gmail.com>
2025-04-29 15:42:29 +0400

    Дз16

--- src/rg/skypro/skyshop/SearchEngine.java
+++ src/rg/skypro/skyshop/SearchEngine.java
@@ -1,5 +1,7 @@
 package rg.skypro.skyshop;
 
+import rg.skypro.skyshop.product.BestResultNotFound;
+
 public class SearchEngine {
 
     private Searchable[] searchables;
@@ -29,5 +31,40 @@ public class SearchEngine {
             }
         }
         return results;
+
     }
+
+    public Searchable searchable(String search) throws BestResultNotFound {
+        if (searchables == null || search == null || search.isEmpty()) {
+            throw new BestResultNotFound("Поисковая строка пуста или список объектов пуст");
+        }
+
+        Searchable bestMatch = null;
+        int maxNumber = 0;
+
+        for (Searchable item : searchables) {
+            if (item != null) {
+                int number = countOccurrences(item.searchTerm(), search);
+                if (number > maxNumber) {
+                    maxNumber = number;
+                    bestMatch = item;
+                }
+            }
+        }
+        if (bestMatch == null) {
+            throw new BestResultNotFound("Не найдено совпадение для строки ' " + search + "'");
+        }
+        return bestMatch;
+    }
+
+    private int countOccurrences(String text, String search) {
+        int index = 0;
+        int count = 0;
+        while ((index = text.indexOf(search, index)) != -1) {
+            count++;
+            index += search.length();
+        }
+        return count;
+    }
+
 }
